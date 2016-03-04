a27a2fa3e9dd5a4362273a939adfe0ddf0e44a0c
Alex Kipriyanova <ambaflo@gmail.com>
2016-03-04 19:18:45 +0400

    step1.0

---
+++ BulkaSite/Controllers/BreadController.cs
@@ -0,0 +1,34 @@
+﻿
+using BulkaSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BulkaSite.Controllers
+{
+    public class BreadController : Controller
+    {
+        // GET: Bread
+        public ActionResult About()
+        {
+            return View();
+        }
+    public ActionResult Receipy()
+    {
+      Bread[] breadList = { new Bread {Name="Бородинский",Receipt="рецепт1" },
+        new Bread {Name="Шведский",Receipt="рецепт2" },
+        new Bread {Name="Подовый",Receipt="рецепт3" } };
+      
+      return View(breadList);
+    }
+
+    public ActionResult RandomFact()
+    {
+      //var breadFact = "Типичная для Челябинска фраза - Дайте мне булку хлеба может поставить" +
+      //  "в тупик продавца любой питерской булочной";
+      return View();
+    }
+  }
+}
---
+++ BulkaSite/Models/Bread.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BulkaSite.Models
+{
+  public class Bread
+  {
+    public String Name { get; set; }
+    public String Receipt { get; set; }
+
+  }
+}
